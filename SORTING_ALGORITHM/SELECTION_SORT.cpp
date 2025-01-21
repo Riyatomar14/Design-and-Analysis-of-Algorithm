623b63ee75288fc438d049d5871ae5ad44323e9e
Riya Tomar <143107173+Riyatomar14@users.noreply.github.com>
2025-01-21 19:13:23 +0530

    Update SELECTION_SORT.cpp

--- SORTING_ALGORITHM/SELECTION_SORT.cpp
+++ SORTING_ALGORITHM/SELECTION_SORT.cpp
@@ -1,1 +1,20 @@
+" Sorting is the process of arranging items systematically, ordered by some criterion.
+  Given an array of n items
+1. Find the largest item x, in the range of [0......n−1]
+2.Swap x with the (n−1)th item
+3. Reduce n by 1 and go to Step 1 "
 
+void selectionSort(int a[], int n) {
+    for (int i = n - 1; i >= 1; i--) {
+        int maxIdx = 0; // Assume the first element is the max
+        for (int j = 1; j <= i; j++) { // Find the max in the range [0, i]
+            if (a[j] > a[maxIdx]) {
+                maxIdx = j;
+            }
+        }
+        // Swap the elements without using a header file
+        int temp = a[i];
+        a[i] = a[maxIdx];
+        a[maxIdx] = temp;
+    }
+}
