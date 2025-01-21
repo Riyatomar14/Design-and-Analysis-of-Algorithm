a14a2f3daf7e5bdfa7ffd7516c42cb9217ef95a5
Riya Tomar <143107173+Riyatomar14@users.noreply.github.com>
2025-01-21 20:17:37 +0530

    Update INSERTION_SORT.cpp

--- SORTING_ALGORITHM/INSERTION_SORT.cpp
+++ SORTING_ALGORITHM/INSERTION_SORT.cpp
@@ -1,1 +1,27 @@
+"
+Similar to how most people arrange a hand of poker cards
+1. Start with one card in your hand
+2. Pick the next card and insert it into its proper sorted order
+3.Repeat previous step for all cards"
+  
+void insertionSort(int a[], int n) {
+    for (int i = 1; i < n; i++) {
+        int next = a[i]; // Element to be inserted
+        int j = i - 1;
 
+        // Shift sorted elements to the right to make space for `next`
+        while (j >= 0 && a[j] > next) {
+            a[j + 1] = a[j];
+            j--;
+        }
+
+        // Insert `next` into the correct position
+        a[j + 1] = next;
+    }
+}
+
+"
+Best Case : O(n)
+Average Case : O(n^2)
+Worst Case : O(n^2)
+"
