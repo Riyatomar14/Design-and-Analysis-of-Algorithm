aa9d73734f9e5cf4d9be5dc6c480941f7acac799
Riya Tomar <143107173+Riyatomar14@users.noreply.github.com>
2025-01-21 23:07:47 +0530

    Update INSERTION_SORT.cpp

--- SORTING_ALGORITHM/INSERTION_SORT.cpp
+++ SORTING_ALGORITHM/INSERTION_SORT.cpp
@@ -5,11 +5,12 @@ Similar to how most people arrange a hand of poker cards
 3.Repeat previous step for all cards"
   
 void insertionSort(int a[], int n) {
-    for (int i = 1; i < n; i++) {
+    for (int i = 1; i < n; i++) { // Starts at index 1, as the element at index 0 is already considered "sorted."
         int next = a[i]; // Element to be inserted
         int j = i - 1;
 
-        // Shift sorted elements to the right to make space for `next`
+        // j >= 0 (to ensure indices are within bounds).
+        // a[j] > next (indicates that the element at a[j] is greater than the element being inserted, so it needs to be shifted).
         while (j >= 0 && a[j] > next) {
             a[j + 1] = a[j];
             j--;
